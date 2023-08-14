512177e3435507424a5cfc7ad7fe8ab352dcd32f
Kraken <h3nry.pas@gmail.com>
2023-08-14 19:55:05 -0400

    Added keyframe editing and export support

--- keyframe_animation.py
+++ keyframe_animation.py
@@ -8,8 +8,8 @@ class Keyframe_animation:
             self.keyframe_list.append(game_object.create_keyframe_from_serialized(e, interval))
         self.elapsed = 0
         self.frame = 0
+        self.interval = interval
         object.set_keyframe(self.keyframe_list[self.frame])
-
 
     def update(self, elapsed):
         self.elapsed += elapsed
@@ -19,3 +19,6 @@ class Keyframe_animation:
             if (self.frame >= len(self.keyframe_list)):
                 self.frame = 0
             self.object.set_keyframe(self.keyframe_list[self.frame])
+
+    def change_animation(self, id_number, serialized_data):
+        self.keyframe_list[id_number] = game_object.create_keyframe_from_serialized(serialized_data, self.interval)
--- main.py
+++ main.py
@@ -21,6 +21,7 @@ g_game_objects = []
 g_playing = True
 g_console_hidden = False
 g_debug_font = pg.font.Font(None, 20)
+g_frame_edit_number = 0
 
 g_animating = True
 
@@ -41,11 +42,18 @@ go.children[0].add_child(game_object.Game_object([-140, 0], 0, 30, "Head"))
 
 go.selected = True
 g_selected = go
+go.read_serialized_data([[557.5, 499], 0, [[[10.0, -130], 4.71238898038469, [[[-100, -30], 4.71238898038469, [[[-60, 0], 5.497787143782138, []]]], [[-99, 30], 1.7934954084936243, [[[-60, 0], 5.497787143782138, []]]], [[0, -30], 3.9, [[[-70, 0], 5.497787143782138, []]]], [[0, 30], 2.5, [[[-70, 0], 0.3, []]]], [[-140, 0], 0, []]]]]])
 
 keyframes = [
     [[557.5, 499], 0, [[[10.0, -130], 4.71238898038469, [[[-100, -30], 4.71238898038469, [[[-60, 0], 5.497787143782138, []]]], [[-99, 30], 1.7934954084936243, [[[-60, 0], 5.497787143782138, []]]], [[0, -30], 3.9, [[[-70, 0], 5.497787143782138, []]]], [[0, 30], 2.5, [[[-70, 0], 0.3, []]]], [[-140, 0], 0, []]]]]],
     [[557.5, 499], 0, [[[10.0, -130], 4.7498889803846955, [[[-100, -30], 4.249888980384617, [[[-60, 0], 5.420287143782126, []]]], [[-99, 30], 3.0859954084935968, [[[-60, 0], 5.497787143782138, []]]], [[0, -30], 2.917500000000021, [[[-70, 0], 1.269601836602659, []]]], [[0, 30], 1.7175000000000167, [[[-70, 0], 0.3, []]]], [[-140, 0], 0, []]]]]],
 ]
+
+
+#dab
+#keyframes = [[[557.5, 499.0], 0.0, [[[10.0, -130.0], 4.720563980384691, [[[-100.0, -30.0], 5.706563980384845, [[[-60.0, 0.0], 5.480892143782135, []]]], [[-99.0, 30.0], 0.8302604084936451, [[[-60.0, 0.0], 5.497787143782138, []]]], [[0.0, -30.0], 4.700815000000107, [[[-70.0, 0.0], 5.945777143782161, []]]], [[0.0, 30.0], 0.9794150000000323, [[[-70.0, 0.0], 0.3, []]]], [[-140.0, 0.0], 0.0, []]]]]], [[557.5, 499.0], 0, [[[10.0, -130.0], 4.7498889803846955, [[[-100.0, -30.0], 4.249888980384617, [[[-60.0, 0.0], 5.420287143782126, []]]], [[-99.0, 30.0], 3.0859954084935968, [[[-60.0, 0.0], 5.497787143782138, []]]], [[0.0, -30.0], 2.917500000000021, [[[-70.0, 0.0], 1.269601836602659, []]]], [[0.0, 30.0], 2.509499999999983, [[[-70.0, 0.0], 0.7464999999999987, []]]], [[-140.0, 0.0], 0, []]]]]]]
+#jumping jacks
+keyframes = [[[557.5, 499.0], 0, [[[0.0, -136.27200000000002], 4.71238898038469, [[[-100.0, -30.0], 3.5773889803845877, [[[-60.0, 0.0], 6.207787143782247, []]]], [[-99.0, 30.0], 2.9009954084936007, [[[-60.0, 0.0], 6.260287143782257, []]]], [[0.0, -30.0], 3.230000000000012, [[[-70.0, 0.0], 0.02960183660261176, []]]], [[0.0, 30.0], 2.98749999999999, [[[-70.0, 0.0], 0.10249999999999863, []]]], [[-140.0, 0.0], 0, []]]]]], [[557.5, 499.0], 0, [[[0.0, -131.008], 4.71238898038469, [[[-100.0, -30.0], 5.459888980384805, [[[-60.0, 0.0], 6.207787143782247, []]]], [[-99.0, 30.0], 0.9059954084936432, [[[-60.0, 0.0], 6.260287143782257, []]]], [[0.0, -30.0], 3.6350000000000033, [[[-70.0, 0.0], 0.02960183660261176, []]]], [[0.0, 30.0], 2.5824999999999987, [[[-70.0, 0.0], 0.10249999999999863, []]]], [[-140.0, 0.0], 0, []]]]]]]
 
 animation = keyframe_animation.Keyframe_animation(go, keyframes, 500)
 
@@ -67,7 +75,8 @@ while g_running:
                  "9 : print serialized data", "< : select next sibling", "/\\ select first child", 
                  "\\/ select parent", "WASD : edit local position (alt to free move)", str(g_selected.local_position),
                   "QE : local rotation, ctrl to fine-tune", str(g_selected.local_rotation),
-                  "frame number: " + str(animation.frame)
+                  "frame number: " + str(animation.frame), "1 and []: override/change frame number " + str(g_frame_edit_number),
+                  "2 : print current animation as serialized data", "3 : snap to frame number " + str(g_frame_edit_number)
                   ]
     utility.stamp_text(debug_str, g_screen_surface, (20,20))
     utility.stamp_text(g_console, g_screen_surface, (SCREEN_SIZE[0]- 260, 0))
@@ -89,11 +98,24 @@ while g_running:
                 g_selected = g_selected.select_parent()   
             if (e.key == pg.K_UP): 
                 g_selected = g_selected.select_first_child()  
-                print(g_selected) 
             if (e.key == pg.K_SPACE):
                 g_playing = not g_playing
             if (e.key == pg.K_1):
-                pass
+                animation.change_animation(g_frame_edit_number, go.serialize_data())
+                keyframes[g_frame_edit_number] = go.serialize_data()
+                console_log("changed frame " + str(g_frame_edit_number) + " to current pose")
+            if (e.key == pg.K_LEFTBRACKET):
+                g_frame_edit_number -= 1
+                if g_frame_edit_number < 0:
+                    g_frame_edit_number = len(animation.keyframe_list) - 1
+            if (e.key == pg.K_RIGHTBRACKET):
+                g_frame_edit_number += 1
+                if g_frame_edit_number >= len(animation.keyframe_list):
+                    g_frame_edit_number = 0
+            if (e.key == pg.K_2):
+                print(keyframes)
+            if (e.key == pg.K_3):
+                go.read_serialized_data(keyframes[g_frame_edit_number])
             if (not k[pg.K_LSHIFT]):
                 if (e.key == pg.K_d):
                     g_selected.local_position[0] += 1
